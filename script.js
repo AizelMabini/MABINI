d9c67eda8e977d9d2184684b4ced5312455447d6
AizelMabini <aizelkim.mabini@cvsu.edu.ph>
2026-08-28 15:41:27 +0800

    Create script.js
    Add JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+
+document.querySelectorAll('nav a').forEach(link => {
+
+    link.addEventListener('click', function(event) {
+
+        event.preventDefault();
+
+        const target = document.querySelector(
+            this.getAttribute('href')
+        );
+
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+
+    });
+
+});
